3f8abe40b6b41d8a50aab72625b3a610db446b15
Jonathan Courtois <50682400+JonathanCourtois@users.noreply.github.com>
2019-07-22 11:02:38 +0200

    Update char-models.jl

--- char-rnn/char-models.jl
+++ char-rnn/char-models.jl
@@ -12,6 +12,7 @@ ctx = CuContext(dev)
 include("char-rnn.jl")
 include("char-lstm.jl")
 include("char-gru.jl")
+include("textCleaner.jl")
 
 cd(@__DIR__)
 
@@ -19,52 +20,6 @@ isfile("input.txt") ||
   download("https://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt",
            "input.txt")
 
-
-
-
-function cleanText(text ; upCase = false ,
-            number = false, accent = true, word = true )
-  if !upCase
-    text = replace(text, text => lowercase)
-  end
-  text = replace(text, r"[;:+†…«»—]" => ' ')
-  text = replace(text, r"[œ]" => "oe")
-  text = replace(text, r"\n|\\|\t" => ' ')
-  text = replace(text, r"[\[{]" => '(')
-  text = replace(text, r"[\]}]" => ')')
-  if !number
-    text = replace(text, r"[1234567890]" => "")
-  end
-  if !accent
-    text = replace(text, r"[âäà]" => 'a')
-    text = replace(text, r"[êëéè]" => 'e')
-    text = replace(text, r"[îïì]" => 'i')
-    text = replace(text, r"[ôöò]" => 'o')
-    text = replace(text, r"[ûüù]" => 'u')
-    text = replace(text, r"[ç]" => 'c')
-  end
-  text = replace(text, r"  " => ' ')
-  text = replace(text, r"   " => ' ')
-  text = replace(text, r"    " => ' ')
-  if word
-    text = replace(text, ' ' => "; ;")
-    text = split(text,r";")
-    alphabet = " ";
-    pas = 1000;
-    to = pas;
-    alphabet = unique([alphabet ; text[1:to]])
-    while to+pas < length(text)
-      alphabet = unique([alphabet ; text[to+1:to+pas]])
-      to += pas
-    end
-    alphabet = unique([alphabet ; text[to+1:length(text)]])
-    alphabet = [alphabet ; "_"]
-  else
-    text = collect(text)
-    alphabet = [unique(text)..., '_']
-  end
-  return text, alphabet
-end
 text = String(read("input.txt"))
 
 text, alphabet = cleanText(text; word = true)
@@ -80,7 +35,7 @@ Xs = collect(partition(batchseq(chunk(text, nbatch), stop), seqlen))
 Ys = collect(partition(batchseq(chunk(text[2:end], nbatch), stop), seqlen))
 
 opt = ADAM(0.01)
-tx, ty = (Xs[5], Ys[5])
+tx, ty = (Xs[1], Ys[1])
 
 function sample(r, alphabet, len; temp = 1)
   r = cpu(r)
@@ -94,23 +49,22 @@ function sample(r, alphabet, len; temp = 1)
   return String(take!(buf))
 end
 
-timeAff = 10;
 
 println(" ################## RNN TEST ################## ")
-r, evacbRnn = charRnn(N, tx, ty, opt)
-@time fTrainRnn(timeAff, r, Xs, Ys, opt, evalcbRnn)
+r, evacbRnn, errorRnn = charRnn(N, tx, ty, opt)
+@time r = fTrainRnn(r, Xs, Ys, opt, evalcbRnn)
 
 sample(r, alphabet, 1000) |> println
 
 println(" ################## LSTM TEST ################## ")
-l, evalcbLstm = charLstm(N, tx, ty, opt)
-@time fTrainLstm(timeAff, l, Xs, Ys, opt, evalcbLstm)
+l, evalcbLstm, errorLstm = charLstm(N, tx, ty, opt)
+@time l = fTrainLstm(l, Xs, Ys, opt, evalcbLstm)
 
 sample(l, alphabet, 1000) |> println
 
 println(" ################## GRU TEST ################## ")
-g, evalcbGru = charGru(N, tx, ty, opt)
-@time fTrainGru(timeAff, g, Xs, Ys, opt, evalcbGru)
+g, evalcbGru, errorGru = charGru(N, tx, ty, opt)
+@time g = fTrainGru(g, Xs, Ys, opt, evalcbGru)
 
 sample(g, alphabet, 1000) |> println
 
